dc5ffc1e17764a65d9f19fca32aa1d9220556dd8
UmangGoyal <goyalumang3008@gmail.com>
2021-06-11 13:15:33 +0530

    Check Available and Ui changes is remaining

--- app/src/main/java/com/example/android/docavailability/Doctor_details.java
+++ app/src/main/java/com/example/android/docavailability/Doctor_details.java
@@ -1,171 +1,180 @@
 package com.example.android.docavailability;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import id.zelory.compressor.Compressor;
-
 
 public class Doctor_details extends AppCompatActivity {
-    EditText doctor_name, Doctor_id, Doctor_speciality;
+    EditText doctor_name, Doctor_id, Doctor_speciality,Doctor_phone_number;
     Button proceed;
     FirebaseFirestore dbroot;
     FirebaseAuth mAuth;
     private ImageView doctor_image;
     ToggleButton tb;
     FirebaseFirestore firebaseFirestore;
-    private Uri imageUri;
     StorageReference storageReference;
     private Bitmap compressor;
+    Uri imageLocationPath;
     String user_id;
+    String HOS_name;
+    String name,id,spec,phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_details);
         Doctor_id = findViewById(R.id.Doctor_id);
         doctor_name = findViewById(R.id.Doctor_name);
+        Doctor_phone_number = findViewById(R.id.Doctor_phone_number);
         mAuth = FirebaseAuth.getInstance();
         Doctor_speciality = findViewById(R.id.Doctor_speciality);
-        proceed = findViewById(R.id.proceed_doctor_details);
+        proceed = findViewById(R.id.edit_doctor_details_);
         dbroot = FirebaseFirestore.getInstance();
-        tb = findViewById(R.id.toggleButton);
+        tb = findViewById(R.id.toggleButton_edit);
         doctor_image = findViewById(R.id.my_avatar);
-        storageReference = FirebaseStorage.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference("USERS");
         user_id = mAuth.getCurrentUser().getUid();
-        doctor_image.setOnClickListener(v ->
-        {
-            boolean result = Utility.checkPermission(Doctor_details.this);
-            if (result) {
-                Toast.makeText(Doctor_details.this, "permission denied ", Toast.LENGTH_SHORT).show();
-                ActivityCompat.requestPermissions(Doctor_details.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-
-            } else {
-                ChoseImage();
-            }
-        });
+        doctor_image.setOnClickListener(this::SelectImage);
         proceed.setOnClickListener(v ->
         {
-            String id = Doctor_id.getText().toString();
-            String name = doctor_name.getText().toString();
-            String spec = Doctor_speciality.getText().toString();
-            if (TextUtils.isEmpty(name) || TextUtils.isEmpty(id) || TextUtils.isEmpty(spec) || doctor_image.getDrawable() == null) {
+            phone = Doctor_phone_number.getText().toString();
+            id = Doctor_id.getText().toString();
+            name = doctor_name.getText().toString();
+            spec = Doctor_speciality.getText().toString();
+            if (TextUtils.isEmpty(name) || TextUtils.isEmpty(id) || TextUtils.isEmpty(spec) || doctor_image.getDrawable() == null || TextUtils.isEmpty(phone)) {
                 Toast.makeText(Doctor_details.this, "Please add all details. ", Toast.LENGTH_SHORT).show();
-            } else {
+            }
+            else
+            {
                 showToast();
-                File file = new File(imageUri.getPath());
-                try {
-                    compressor = new Compressor(Doctor_details.this)
-                            .setMaxHeight(125)
-                            .setMaxWidth(125)
-                            .setQuality(50)
-                            .compressToBitmap(file);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                compressor.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-                byte[] thumb = byteArrayOutputStream.toByteArray();
-
-                UploadTask image_path = storageReference.child("USERS").child(user_id).child(doctor_name.getText().toString()+ ".jpg").putBytes(thumb);
-
-                image_path.addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        storeDoctorData(task);
-                    } else {
-                        Toast.makeText(Doctor_details.this, "Please Try again later", Toast.LENGTH_LONG).show();
-                    }
-                });
-
+                uploadImage(v);
             }
 
         });
     }
-
-    private void storeDoctorData(Task<UploadTask.TaskSnapshot> taskSnapshotTask) {
-        Uri download_uri;
-        String down_uri;
-        Map<String, Object> doctors = new HashMap<>();
-        doctors.put("name", doctor_name.getText().toString());
-        doctors.put("id", Doctor_id.getText().toString());
-        doctors.put("spec", Doctor_speciality.getText().toString());
-        if (taskSnapshotTask != null) {
-            down_uri = Objects.requireNonNull(Objects.requireNonNull(Objects.requireNonNull(taskSnapshotTask.getResult()).getMetadata()).getReference()).getDownloadUrl().toString();
-            doctors.put("image", down_uri);
-        } else {
-            download_uri = imageUri;
-            doctors.put("image", download_uri.toString());
+    public void SelectImage(View view)
+    {
+        try{
+            Intent i = new Intent();
+            i.setType("image/*");
+            i.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(i,71);
         }
-
-        if (tb.getText().toString().equals("ON")) {
-            doctors.put("Available", true);
-        } else {
-            doctors.put("Available", false);
+        catch(Exception e)
+        {
+            Toast.makeText(Doctor_details.this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-        dbroot.collection("USERS").document(user_id).collection("Doctor_Details").add(doctors).addOnCompleteListener(task -> Toast.makeText(Doctor_details.this, "Successfully added. ", Toast.LENGTH_SHORT).show());
-        Intent i = new Intent(Doctor_details.this, Hospital_details.class);
-        startActivity(i);
-        finish();
-    }
-
-    private void ChoseImage() {
-        CropImage.activity()
-                .setGuidelines(CropImageView.Guidelines.ON)
-                .setAspectRatio(1, 1)
-                .start(Doctor_details.this);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                imageUri = result.getUri();
-                doctor_image.setImageURI(imageUri);
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
+        if (requestCode == 71 && resultCode==RESULT_OK && data!=null&&data.getData()!=null)
+        {
+            imageLocationPath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imageLocationPath);
+                doctor_image.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
         }
-
     }
+    private void uploadImage(View view) {
+        try
+        {
+            if(imageLocationPath!=null)
+            {
+                HOS_name = mAuth.getUid();
+                String nameOfimage = doctor_name.getText().toString() +  ".jpeg";
+                StorageReference image_ref=storageReference.child(HOS_name).child(nameOfimage);
+                UploadTask uploadTask=image_ref.putFile(imageLocationPath);
+                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if(!task.isSuccessful())
+                        {
+                            throw Objects.requireNonNull(task.getException());
+                        }
+                        return image_ref.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(task -> {
+                    if(task.isSuccessful())
+                    {
+                        Map<String,Object> hashMap= new HashMap<>();
+                        hashMap.put("image",task.getResult().toString());
+                        hashMap.put("name",name);
+                        hashMap.put("id",id);
+                        hashMap.put("phone",phone);
+                        hashMap.put("spec",spec);
+                        if (tb.getText().toString().equals("Available")) {
+                            hashMap.put("Available", true);
+                        } else {
+                            hashMap.put("Available", false);
+                        }
+                        dbroot.collection("USERS").document(mAuth.getUid()).collection("Doctor_Details").document(name).set(hashMap).addOnCompleteListener(t -> Toast.makeText(Doctor_details.this, "Successfully added. ", Toast.LENGTH_SHORT).show());
+                        Intent i = new Intent(Doctor_details.this, Hospital_details.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else if(!task.isSuccessful())
+                    {
+                        Toast.makeText(Doctor_details.this,task.getException().toString(),Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+            }
+            else
+            {
+                Toast.makeText(this,"Please enter Valid Details",Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+    }
+    private String  Extension(Uri uri) {
+        try {
+            ContentResolver object = getContentResolver();
+            MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+            return mimeTypeMap.getExtensionFromMimeType(object.getType(uri));
+        } catch (Exception e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+        return null;
+    }
+
 
     private Toast mToastToShow;
 
@@ -187,36 +196,3 @@ public class Doctor_details extends AppCompatActivity {
     }
 }
 
-class Utility {
-    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public static boolean checkPermission(final Context context) {
-        int currentAPIVersion = Build.VERSION.SDK_INT;
-        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
-                    alertBuilder.setCancelable(true);
-                    alertBuilder.setTitle("Permission necessary");
-                    alertBuilder.setMessage("External storage permission is necessary");
-                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                        }
-                    });
-                    AlertDialog alert = alertBuilder.create();
-                    alert.show();
-                } else {
-                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                }
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            return true;
-        }
-    }
-}
